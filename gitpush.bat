53bcb47244c77e6ad84d5f11c00968fe5f34eac5
rweil55 <github@shaw-weil.com>
2024-10-12 10:14:29 -0400

    2024-10-12

--- gitpush.bat
+++ gitpush.bat
@@ -3,8 +3,7 @@ cd /inetpub/wwwroot/w/brooke
 
 
 git add .
-git commit -m "from indesign 2024-10-11 18:07" --no-edit
-git pull
+git commit -m "from indesign 2024-10-12 14:14" --no-edit
 git push https://github.com/rweil55/web-brooke main
 
 rem wait for one minute to allow git hub to get its act together
