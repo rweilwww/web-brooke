e73c27ae0af8f13a895577da8997ff897d5a89d8
rweilwww <github@shaw-weil.com>
2025-10-24 10:09:49 -0400

    from inDesign 2025-10-24 14:09

--- gitpush.bat
+++ gitpush.bat
@@ -3,7 +3,7 @@ cd /inetpub/wwwroot/w/brooke
 
 
 git add .
-git commit -m "from inDesign 2025-10-23 01:19" --no-edit
+git commit -m "from inDesign 2025-10-24 14:09" --no-edit
 git push --force --set-upstream origin main
 
 rem wait for one minute to allow git hub to get its act together
